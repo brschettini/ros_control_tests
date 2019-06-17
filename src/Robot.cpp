4eb87d008585c843c7e09d4f1bdbcfccaf51df51
brschettini <brschettini@gmail.com>
2019-06-17 15:47:17 -0300

    Create a quick example for the controller manager working with read() and write() methods

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,6 +1,9 @@
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
+#include <controller_manager/controller_manager.h>
 #include <hardware_interface/robot_hw.h>
+#include <ros/ros.h>
+
 
 
 class Robot : public hardware_interface::RobotHW {
@@ -14,16 +17,44 @@ class Robot : public hardware_interface::RobotHW {
   public:
     Robot() {
         // connect and register the joint state interface
-        hardware_interface::JointStateHandle state_handle("Maxon", &joint_pos_[0], &joint_vel_[0], &joint_eff_[0]);
-        jnt_state_interface.registerHandle(&state_handle);
+        hardware_interface::JointStateHandle state_handle("A", &joint_pos_[0], &joint_vel_[0], &joint_eff_[0]);
+        jnt_state_interface.registerHandle(state_handle);
 
         registerInterface(&jnt_state_interface);
 
         // connect and register the joint position interface
-        hardware_interface::PositionJointInterface pos_handle(jnt_state_interface.getHandle("Maxon"),&joint_cmd_[0]);
-        jnt_pos_interface.registerHandle(&pos_handle);
+        
+        hardware_interface::JointHandle pos_handle(jnt_state_interface.getHandle("A"), &joint_cmd_[0]);
+        jnt_pos_interface.registerHandle(pos_handle);
 
         registerInterface(&jnt_pos_interface);
     }
 
+    void write() {
+      std::cout << "This is a write method" << std::endl;
+    }
+
+    void read() {
+      std::cout << "This is a read method" << std::endl;
+      // This will update jnt_pos_, jnt_vel_ and jnt_eff_ variables (talking with the driver)
+    }
 };
+
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "robot_test");
+
+  Robot robot;
+  controller_manager::ControllerManager cm(&robot);
+  ros::Duration period(1.0);
+  while (ros::ok()) {
+    robot.read();
+    cm.update(ros::Time::now(), ros::Duration(1.0));
+    robot.write();
+    std::cout << "This is a controller manager loop (not sure!)" << std::endl;
+    sleep(1);
+  }
+  
+  ros::spin();
+}
